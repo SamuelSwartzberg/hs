9a86bf6fc5bd53459709fd656e0bddac265be085
Sam Swartzberg <me@samswartzberg.com>
2023-08-09 18:45:25 +0000

    more deletions

--- items-managers/items/string/path/absolute-path/path-existance/extant-path/dir/parent-dir/init.lua
+++
@@ -1,7 +0,0 @@
-local rrq = bindArg(relative_require, "items-managers.items.string.path.absolute-path.path-existance.extant-path.dir.parent-dir")
-
-rrq("grandparent-dir")
-rrq("parent-but-not-grandparent-dir")
-rrq("project-dir")
-rrq("managed-date-project-dir")
-rrq("parent-dir")
